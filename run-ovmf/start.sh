6b4d272c3381a03573180def800c1050f389c6fc
Yyx-laptop <3454523412@qq.com>
2024-11-22 08:47:46 +0800

    2

--- run-ovmf/start.sh
+++
@@ -1,4 +0,0 @@
-qemu-system-x86_64 \
-	-pflash bios.bin \
-	-hda fat:rw:hda-contents \
-	-net none
---
+++ target/scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+qemu-system-x86_64 \
+	-bios /usr/share/ovmf/OVMF.fd \
+	-cpu kvm64,+smep,+smap \
+	-smp sockets=1,dies=1,cores=4,threads=2 \
+	-m 4G \
+	--machine q35 \
+	-drive file=./target/kernel.iso,format=raw,index=0,media=diskkernel.iso,format=raw,index=0,media=disk
