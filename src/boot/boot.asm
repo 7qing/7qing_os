880204c6d11301c5d8af18e0bd369b1e48f65d03
yyx-pc <3454523412@qq.com>
2024-12-11 20:44:52 +0800

    day2

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,4 +1,4 @@
-ORG 0
+ORG 0x7c00
 BITS 16
 
 _start:
@@ -8,21 +8,56 @@ _start:
 times 33 db 0
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli     ;Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables Interrupts
 
-    mov si, message
-    call print
-    jmp $
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov  eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:  ;cs
+    dw 0xffff
+    dw 0
+    dw 0
+    db 0x9a
+    db 11001111b
+    db 0
+
+; offset 0x10
+gdt_data: ;ds ss es fs  gs
+    dw 0xffff
+    dw 0
+    dw 0
+    db 0x92
+    db 11001111b
+    db 0 
+
+gdt_end:
+
+
+gdt_descriptor:
+    dw gdt_end- start -1
+    dd gdt_start
+
+    
 
 print:
     .loop:
@@ -35,8 +70,7 @@ print:
 .done:
     ret
 
-message: db 'hello,world',0
-
 times 510- ($ - $$) db 0
+
 db 0x55
 db 0xAA
