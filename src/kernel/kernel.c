f4ea7eab74cae588fe08ff6fbb9122c3ff743415
Yan Yixin <88801828+7qing@users.noreply.github.com>
2025-03-04 21:14:12 +0800

    1

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,14 +2,15 @@
  * @Author: Yan Yixin 88801828+7qing@users.noreply.github.com
  * @Date: 2025-02-22 14:07:07
  * @LastEditors: Yan Yixin 88801828+7qing@users.noreply.github.com
- * @LastEditTime: 2025-03-04 17:59:06
+ * @LastEditTime: 2025-03-04 19:40:41
  * @FilePath: /v2_7qing_os/src/kernel/main.c
  * @Description: 这个是c语言启动的入口
  */
 
+
 #include <stddef.h>
 #include <stdint.h>
-
+#include "memory/map.h"
 #include "boot/multiboot2.h"
 
 static uint32_t *framebuffer_base; /* 32 bit color */
@@ -17,14 +18,14 @@ static uint32_t framebuffer_width;
 static uint32_t framebuffer_height;
 
 
-static int boot_get_frame_buffer(multiboot_uint8_t *mbi)
+static void* boot_get_frame_buffer(multiboot_uint8_t *mbi)
 {
     struct multiboot_tag_framebuffer *fb_info = NULL;
     struct multiboot_tag *tag = (struct multiboot_tag *) (mbi + 8);
 
     /* find framebuffer tag */
     if (tag == NULL) {
-        return -1;
+        return NULL;
     }
 
     while (tag->type != MULTIBOOT_TAG_TYPE_END) {
@@ -38,17 +39,19 @@ static int boot_get_frame_buffer(multiboot_uint8_t *mbi)
     }
 
     if (fb_info == NULL) {
-        return -1;
+        return NULL;
     }
 
     framebuffer_base = (uint32_t*) fb_info->common.framebuffer_addr;
     framebuffer_height = fb_info->common.framebuffer_height;
     framebuffer_width = fb_info->common.framebuffer_width;
-
-    return 0;
+    
+    return fb_info;
 }
 
-extern void kernel_main(void *addr) {
-    
+extern void kernel_main(multiboot_uint8_t *mbi) {
+  void *addr;
+  addr = boot_get_frame_buffer(mbi);
+  
   while(1);
 }
